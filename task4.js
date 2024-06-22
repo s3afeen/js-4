943152cedf12f368def376b6c667b95a7e75a2ef
s3afeen <zaidsafeen30@gmail.com>
2024-06-23 00:45:44 +0300

    js-4

---
+++ task4.js
@@ -0,0 +1,74 @@
+//----------------1
+// document.getElementById("123").innerHTML  = "Welcome to DOM Manipulation!";
+
+
+
+//----------------2
+// let  task = document.getElementsByClassName("item");
+// for (let z = 0; z < task.length; z++)
+//     {
+//         task[z].innerHTML="Class item";
+    //}
+
+
+
+//----------------3
+// let zaid = document.getElementsByTagName("p");
+// for (let z in zaid )
+//     {
+//         zaid[z].innerHTML="Paragraph Element"
+//    }
+
+
+
+//----------------4
+// document.querySelector("p").style.backgroundColor = "red"
+
+
+//----------------5
+// var x = document.querySelectorAll("li");
+// for(let z = 0 ; z < x.length ; z++)
+// {
+//     x[z].style.color  = "red"
+//     x[z].style.fontSize  = "40px"
+// }
+
+
+
+//----------------6
+// let y = document.querySelector("#footer")
+// document.write(y)
+
+
+
+//----------------7
+// let r = document.querySelectorAll(".box");
+// console.log(r)
+
+
+
+//----------------8
+// let t = document.querySelector(".container")
+// console.log(t)
+
+
+//----------------9
+// const adminElement = document.querySelector('[data-role="admin"]');
+// adminElement.textContent = "Admin Role";
+
+
+
+//----------------10
+// const section = document.getElementById('mySection');
+// const spans = section.getElementsByTagName('span');
+// const h1 = section.getElementsByTagName('h1');
+
+// for (let i = 0; i < spans.length; i++) {
+//     spans[i].textContent = "Span Element";
+// }
+
+// for (let i = 0; i < h1.length; i++) {
+//     h1[i].textContent = "Span Element";
+// }
+
+
